966816bbc994b95bc66d0e94617e50a211b3dc13
Jeff Stewart <jeff.stewart@gov.bc.ca>
2017-10-04 09:51:27 -0700

    troubleshooting ldap

--- openshift/config/config.php
+++ openshift/config/config.php
@@ -57,9 +57,10 @@ return array(
 	// then please check your error-logs - either in your hosting provider admin panel or in some /logs directory
 	// on your webspace.
 	// LimeSurvey developers: Set this to 2 to additionally display STRICT PHP error messages and get full access to standard templates
-		'debug'=>0,
+		'debug'=>2,
 		'debugsql'=>0, // Set this to 1 to enanble sql logging, only active when debug = 2
 		// Update default LimeSurvey config here
+		'enableLdap'=>true,
 	)
 );
 /* End of file config.php */
