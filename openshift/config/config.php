56508b2e757752a49abfd05d5147f15e8bf9e0df
Jeff Stewart <jeff.stewart@gov.bc.ca>
2017-10-04 10:30:55 -0700

    allowing for use of env variable to control debug for limesurvey

--- openshift/config/config.php
+++ openshift/config/config.php
@@ -57,10 +57,9 @@ return array(
 	// then please check your error-logs - either in your hosting provider admin panel or in some /logs directory
 	// on your webspace.
 	// LimeSurvey developers: Set this to 2 to additionally display STRICT PHP error messages and get full access to standard templates
-		'debug'=>2,
+		'debug'=> getenv( "LIMESURVEY_DEBUG" ),
 		'debugsql'=>0, // Set this to 1 to enanble sql logging, only active when debug = 2
 		// Update default LimeSurvey config here
-		'enableLdap'=>true,
 	)
 );
 /* End of file config.php */
